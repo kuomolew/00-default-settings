bcd690d7d4ba3adff015ac5838be594dc9b50d51
kuomolew <kuomolew@gmail.com>
2019-10-19 18:50:42 +0300

    before autoprefix

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,5 @@
 var gulp = require("gulp");
 var sass = require("gulp-sass");
-const autoprefixer = require('gulp-autoprefixer');
 
 gulp.task("compile", () => {
   return gulp.src("./css/style.scss")
